bd37523c2d6af29498aa6c6439ee5991d089957d
Gus Workman <gworkman@users.noreply.github.com>
2025-09-12 17:39:47 +0200

    assert gpio set interrupts succeeds

--- lib/name_badge/renderer.ex
+++ lib/name_badge/renderer.ex
@@ -20,8 +20,8 @@ defmodule NameBadge.Renderer do
     {:ok, btn_1} = GPIO.open(@btn_1, :input)
     {:ok, btn_2} = GPIO.open(@btn_2, :input)
 
-    GPIO.set_interrupts(btn_1, :both)
-    GPIO.set_interrupts(btn_2, :both)
+    :ok = GPIO.set_interrupts(btn_1, :both)
+    :ok = GPIO.set_interrupts(btn_2, :both)
 
     VintageNet.subscribe(@wlan0_property)
 
