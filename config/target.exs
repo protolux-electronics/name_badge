dd17c789041954b24d0a13fbb36d2bcb73eb7d85
Benjamin Milde <benni@kobrakai.de>
2025-09-13 17:42:03 +0200

    Move wifi bringup to power manager

--- config/target.exs
+++ config/target.exs
@@ -28,7 +28,7 @@ config :nerves_ssh,
   daemon_option_overrides: [
     {:pwdfun, &NameBadge.ssh_check_pass/2},
     {:auth_method_kb_interactive_data, &NameBadge.ssh_show_prompt/3}
-    ]
+  ]
 
 # Configure the network using vintage_net
 #
@@ -37,6 +37,7 @@ config :nerves_ssh,
 # See https://github.com/nerves-networking/vintage_net for more information
 config :vintage_net,
   regulatory_domain: "00",
+  power_managers: [{NameBadge.Wifi, [gpio: "WIFI_EN", ifname: "wlan0"]}],
   config: [
     {"usb0", %{type: VintageNetDirect}},
     {"eth0",
